1ad3c2b4efaf524aed7ae54a2478b8f361e69000
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2016-04-01 14:52:08 +0200

    rx_meta param removed, rssi is part of d7asp_result_t now

--- stack/apps/d7ap_test/d7ap_test.c
+++ stack/apps/d7ap_test/d7ap_test.c
@@ -33,7 +33,7 @@
 
 #include "d7ap_stack.h"
 #include "dll.h"
-
+#include "alp_cmd_handler.h"
 
 #ifdef HAS_LCD
 #include "hwlcd.h"
@@ -74,10 +74,10 @@ void execute_sensor_measurement() {
 }
 
 void on_unsollicited_response_received(d7asp_result_t d7asp_result,
-                                      uint8_t *alp_command, uint8_t alp_command_size,
-                                      hw_rx_metadata_t* rx_meta)
+                                      uint8_t *alp_command, uint8_t alp_command_size)
 {
-	DPRINT("Unsol resp %d\n", rx_meta->rssi);
+  alp_cmd_handler_output_unsollicited_response(d7asp_result, alp_command, alp_command_size);
+  DPRINT("Unsol resp %d\n", d7asp_result.rssi);
 }
 
 void init_user_files() {
@@ -190,11 +190,3 @@ void bootstrap() {
     sched_register_task((&execute_sensor_measurement));
     timer_post_task_delay(&execute_sensor_measurement, REPORTING_INTERVAL_TICKS);
 }
-
-
-//__attribute__( (naked) )
-//void HardFault_Handler(void)
-//{
-//	__asm__("BKPT");
-//	while(1);
-//}
